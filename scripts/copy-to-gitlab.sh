c63932a6fb2cf874c377530a9d10ada410c7f58b
Olegzandr VD <alex@oresoftware.com>
2018-08-14 03:00:53 -0700

    pdev:'set'

--- scripts/copy-to-gitlab.sh
+++ scripts/copy-to-gitlab.sh
@@ -2,17 +2,15 @@
 
 set -e;
 
-root="$HOME/WebstormProjects/oresoftware/dell-xps-backup"
+mkdir -p "$HOME/oresoftware_backups";
+
+root="$HOME/oresoftware_backups/dell-xps-backup"
 
 cd "$root";
 
-git rm -rf "$root/WebstormProjects" || { echo "Could not remove folder."; };
-
-rm -rf "$root/WebstormProjects"
-
 mkdir -p "$root/WebstormProjects"
 
-rsync -r --exclude=".git" --exclude="node_modules" "$HOME/WebstormProjects/" "$root/WebstormProjects/"
+rsync -r --checksum --exclude=".git" --exclude="node_modules" "$HOME/WebstormProjects/" "$root/WebstormProjects/"
 
 hg add
 hg commit -m "auto-commit"
